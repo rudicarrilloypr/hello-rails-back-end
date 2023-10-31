dd1ce434dbdc8134b3c1bab4681d857da03bba80
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-30 20:13:06 -0600

    Add functional back end and setup

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,7 @@
+class MessagesController < ApplicationController
+    def random_greeting
+      message = Message.order("RANDOM()").first
+      render json: { greeting: message.greeting }
+    end
+  end
+  
